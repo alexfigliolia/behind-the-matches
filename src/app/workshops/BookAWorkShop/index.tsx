fdd9230a8272097c041d1ca5ecaf3c39dae48884
Alex Figliolia <alexfigliolia@gmail.com>
2025-05-15 11:12:16 -0400

    Wire workshop booking up to netlify

--- src/app/workshops/BookAWorkShop/index.tsx
+++ src/app/workshops/BookAWorkShop/index.tsx
@@ -1,22 +1,33 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 import { BottomSheet } from "Components/BottomSheet";
 import { Button } from "Components/Button";
 import { Closer } from "Components/Closer";
+import { Form, FormCTRL } from "Components/Form";
 import { Input } from "Components/Input";
+import { ModalFormFooter } from "Components/ModalFormFooter";
 import { TextArea } from "Components/TextArea";
 import { Suspended } from "HOCs/Suspended";
 import { useModalToggle } from "Hooks/useModalToggle";
+import { useNetlifyForm } from "Hooks/useNetlifyForm";
 import { useReplaceSearchParams } from "Hooks/useReplaceSearchParams";
 import { Propless } from "Types/React";
 import "./styles.scss";
 
 export const BookAWorkShop = Suspended((_: Propless) => {
+  const form = useRef<FormCTRL>(null);
   const [open, setOpen] = useState(false);
 
   const params = useSearchParams();
   const replace = useReplaceSearchParams();
+
+  const {
+    onSubmit,
+    setState: _setState,
+    resetState,
+    ...status
+  } = useNetlifyForm();
 
   const openSheet = useCallback(() => {
     setOpen(true);
@@ -25,9 +36,11 @@ export const BookAWorkShop = Suspended((_: Propless) => {
   const close = useCallback(() => {
     setOpen(false);
     setTimeout(() => {
+      resetState();
       replace(p => p.delete("book"));
+      form?.current?.clear?.();
     }, 400);
-  }, [replace]);
+  }, [replace, resetState]);
 
   const toggle = useModalToggle(openSheet, close);
 
@@ -43,13 +56,26 @@ export const BookAWorkShop = Suspended((_: Propless) => {
     <BottomSheet open={open} close={toggle.close} className="book-a-workshop">
       <Closer aria-label="Close Window" onClick={close} />
       <h2>Book Your Event</h2>
-      <form>
-        <Input required label="Name" type="text" />
-        <Input required label="Email" type="email" />
-        <Input required label="Date" type="date" />
-        <TextArea required label="Message" />
-        <Button text="Book!" />
-      </form>
+      <Form ref={form} name="book-an-event" onSubmit={onSubmit}>
+        <input type="hidden" name="form-name" value="book-an-event" />
+        <Input required name="name" label="Name" type="text" />
+        <Input required name="email" label="Email" type="email" />
+        <Input required name="date" label="Date" type="date" />
+        <TextArea required name="details" label="Message" />
+        <ModalFormFooter
+          {...status}
+          onClose={toggle.close}
+          resetState={resetState}
+          successText="Someone from our team will reach out to you shortly"
+          showStatus={status.success || !!status.error || status.loading}
+          actions={
+            <Fragment>
+              <Button type="button" text="Cancel" onClick={toggle.close} />
+              <Button text="Submit" />
+            </Fragment>
+          }
+        />
+      </Form>
     </BottomSheet>
   );
 });
