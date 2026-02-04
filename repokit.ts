82eb5d8ee258a2f0ff30d72ac866ba270cd9c325
Alex Figliolia <alexfigliolia@gmail.com>
2026-02-03 18:29:27 -0800

    Upgrade repokit

--- repokit.ts
+++ repokit.ts
@@ -1,9 +1,5 @@
 import { RepoKitConfig } from "@repokit/core";
 
-/**
- * Please fill out this config file with your desired
- * repokit settings
- */
 export const RepoKit = new RepoKitConfig({
   project: "BHM",
   commands: {
